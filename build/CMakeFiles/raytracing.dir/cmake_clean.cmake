f36a4524c0fd1c566423e84c2054657c1162767e
Arafath3 <arafathreja0@gmail.com>
2025-04-24 01:14:50 +1000

    first commit

---
+++ build/CMakeFiles/raytracing.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracing.dir/src/main.cpp.obj"
+  "CMakeFiles/raytracing.dir/src/main.cpp.obj.d"
+  "libraytracing.dll.a"
+  "raytracing.exe"
+  "raytracing.exe.manifest"
+  "raytracing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,84 @@
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
+#include <iostream>
+using namespace std;
+
+
+
+bool hit_sphere(const point3& center, double redius, const ray& r){
+    /* 
+    refer to image calculating_sphere_part 1 and 2 + calculting_t
+    
+    */
+    vec3 oc = center - r.origin();  // (C - P) or (C - Q), the vector from ray origin to sphere center
+    auto a = dot(r.direction(), r.direction()); // d . d
+    auto b = -2.0 * dot(r.direction(), oc); // −2d⋅(C−Q)
+    auto c = dot(oc, oc) - redius * redius; // (C − Q) · (C − Q) − r²
+
+    auto discriminant = b*b - 4*a*c; // b² - 4ac
+    return (discriminant >= 0);
+}
+
+// returning color of the ray
+color ray_color(const ray& r) {
+    if(hit_sphere(point3(0, 0, -1), .5, r)){
+        return color(1,0,0);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
+
+
+int main()
+{
+    // image
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // Calculating the image height, and ensuring that it's at least 1.
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // Calculating the vector across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0,0);
+    auto viewport_v = vec3(0,-viewport_height,0);
+
+    // Calculating the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculting the location of the top left pixel
+    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+
+    // Render
+    cout << "P3\n"
+         << image_width << " " << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++)
+    {   
+        clog << "\rScanlines remaining: " << (image_height -j) << " " << flush;
+        for (int i = 0; i < image_width; i++)
+        {
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
+            write_color(cout, pixel_color);
+        }
+    }
+    clog << "\rDone.                 \n";
+    return 0;
+}
